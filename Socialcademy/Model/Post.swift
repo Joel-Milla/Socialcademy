ef0d45565cac0eedf47ca8ad546809c35a3850bc
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-05 12:27:18 -0600

    Add a post structure that will contain the posts of the project.

--- Socialcademy/Model/Post.swift
+++ Socialcademy/Model/Post.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct Post: Identifiable {
+    var title: String
+    var content: String
+    var authorName: String
+    var timestamp = Date()
+    var id = UUID()
+}
