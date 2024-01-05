b5131a5d2044dc1207dd6a78a7bde876e07b5be6
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-05 12:08:20 -0600

    Initial Commit

---
+++ Socialcademy/SocialcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialcademyApp.swift
+//  Socialcademy
+//
+//  Created by Joel Alejandro Milla Lopez on 05/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SocialcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
