cd2e4faa8abc6f053c86305138c7368b0265554b
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-06 15:48:37 -0600

    Improve AuthView by creating a sign in and create account and connected it with AuthViewModel to update information with firebase.

--- Socialcademy/ViewModels/AuthViewModel.swift
+++ Socialcademy/ViewModels/AuthViewModel.swift
@@ -10,22 +10,18 @@ import Foundation
 @MainActor
 class AuthViewModel: ObservableObject {
     @Published var isAuthenticated = false
-    @Published var email = ""
-    @Published var password = ""
     private let authService = AuthService()
     
     init() {
         authService.$isAuthenticated.assign(to: &$isAuthenticated)
     }
     
-    func signIn() {
-        Task {
-            do {
-                try await authService.signIn(email: email, password: password)
-            } catch {
-                print("[AuthViewModel] Error while singin in: \(error)")
-            }
-        }
+    func makeSignInViewModel() -> SignInViewModel {
+        return SignInViewModel(action: authService.signIn(email:password:))
+    }
+    
+    func makeCreateAccountViewModel() -> CreateAccountViewModel {
+        return CreateAccountViewModel(action: authService.createAccount(name:email:password:))
     }
 }
 
--- Socialcademy/Views/AuthView.swift
+++ Socialcademy/Views/AuthView.swift
@@ -14,13 +14,45 @@ struct AuthView: View {
         if authViewModel.isAuthenticated {
             MainTabView()
         } else {
+            NavigationStack {
+                SignIn(authViewModel: authViewModel.makeSignInViewModel()) {
+                    NavigationLink("Create account") {
+                        CreateAccount(authViewModel: authViewModel.makeCreateAccountViewModel())
+                    }
+                }
+            }
+        }
+    }
+}
+
+// Sign in and create acocunt views
+private extension AuthView {
+    struct SignIn<Footer: View>: View {
+        @StateObject var authViewModel: AuthViewModel.SignInViewModel
+        @ViewBuilder let footer: () -> Footer
+        
+        var body: some View {
             Form {
                 TextField("Email", text: $authViewModel.email)
                 SecureField("Password", text: $authViewModel.password)
-                Button("Sign In") {
-                    authViewModel.signIn()
-                }
+                Button("Sign In", action: authViewModel.submit)
+                footer()
             }
+            .navigationTitle("Sign In")
+        }
+    }
+
+    struct CreateAccount: View {
+        @StateObject var authViewModel: AuthViewModel.CreateAccountViewModel
+        
+        var body: some View {
+            Form {
+                TextField("Name", text: $authViewModel.name)
+                TextField("Email", text: $authViewModel.email)
+                SecureField("Password", text: $authViewModel.password)
+                Button("Create Account", action: authViewModel.submit)
+            }
+            .navigationTitle("Create Account")
         }
     }
 }
