6b8d6c2fbb9ffdbbc1d584696b954ba3aed816a7
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-06 19:00:17 -0600

    show a progress view when the app is waiting for a response.

--- Socialcademy/Supporting Views/PrimaryButtonStyle.swift
+++ Socialcademy/Supporting Views/PrimaryButtonStyle.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct PrimaryButtonStyle: ButtonStyle {
+    @Environment(\.isEnabled) private var isEnabled
     func makeBody(configuration: Configuration) -> some View {
-        configuration.label
+        
+        Group {
+            if isEnabled {
+                configuration.label
+            } else {
+                ProgressView()
+            }
+        }
             .padding()
             .frame(maxWidth: .infinity)
             .foregroundStyle(.white)
             .background(Color.accentColor)
             .clipShape(RoundedRectangle(cornerRadius: 10))
+            .animation(.default, value: isEnabled)
     }
 }
