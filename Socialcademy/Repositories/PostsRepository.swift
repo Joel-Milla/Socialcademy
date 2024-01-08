3698b589303bb751381d85506fb099d830ca660b
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-08 12:42:43 -0600

    Create external file to manage the extensions that are being used in all the repositories.

--- Socialcademy/Repositories/PostsRepository.swift
+++ Socialcademy/Repositories/PostsRepository.swift
@@ -120,31 +120,6 @@ private extension Post {
     }
 }
 
-private extension Query {
-    func getDocuments<T: Decodable>(as type: T.Type) async throws -> [T] {
-        let snapshot = try await getDocuments()
-        return snapshot.documents.compactMap { document in
-            try! document.data(as: type)
-        }
-    }
-}
-
-private extension DocumentReference {
-    func setData<T: Encodable>(from value: T) async throws {
-        return try await withCheckedThrowingContinuation { continuation in
-            // Method only throws if there’s an encoding error, which indicates a problem with our model.
-            // We handled this with a force try, while all other errors are passed to the completion handler.
-            try! setData(from: value) { error in
-                if let error = error {
-                    continuation.resume(throwing: error)
-                    return
-                }
-                continuation.resume()
-            }
-        }
-    }
-}
-
 
 
 #if DEBUG
