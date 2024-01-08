c0d6ef481c8e52718e6e7b154f4a2e88c082afc3
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-08 14:58:56 -0600

    Add the option to create a new comment for a post.

--- Socialcademy/Views/CommentsList.swift
+++ Socialcademy/Views/CommentsList.swift
@@ -9,7 +9,8 @@ import SwiftUI
 
 struct CommentsList: View {
     @StateObject var commentsViewModel: CommentsViewModel
-
+    
+    @State var createNewPost = false
     var body: some View {
         Group {
             switch commentsViewModel.comments {
@@ -25,18 +26,78 @@ struct CommentsList: View {
                 }
             case .empty:
                 EmptyListView(title: "No comments", message: "Be the first to leave a comment.")
+                    .toolbar {
+                        ToolbarItem(placement: .topBarTrailing) {
+                            Button {
+                                createNewPost = true
+                            } label: {
+                                Image(systemName: "paperplane")
+                            }
+                        }
+                    }
             case let .loaded(comments):
                 List(comments) { comment in
                     CommentRow(comment: comment)
                 }
                 .animation(.default, value: comments)
+                .toolbar {
+                    ToolbarItem(placement: .topBarTrailing) {
+                        Button {
+                            createNewPost = true
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                }
             }
         }
         .navigationTitle("Comments")
         .navigationBarTitleDisplayMode(.inline)
+        .sheet(isPresented: $createNewPost) {
+            NewCommentForm(formViewModel: commentsViewModel.makeNewCommentViewModel())
+        }
+    }
+}
+
+private extension CommentsList {
+    struct NewCommentForm: View {
+        @StateObject var formViewModel: FormViewModel<Comment>
+        @Environment(\.dismiss) private var dismiss
+        
+        var body: some View {
+            Form {
+                Section("Content") {
+                    TextEditor(text: $formViewModel.content)
+                        .multilineTextAlignment(.leading)
+                }
+                
+                Button(action: formViewModel.submit, label: {
+                    if formViewModel.isWorking {
+                        ProgressView()
+                    } else {
+                        Text("Create Comment")
+                    }
+                })
+                .font(.headline)
+                .frame(maxWidth: .infinity)
+                .foregroundStyle(.white)
+                .padding()
+                .listRowBackground(Color.accentColor)
+            }
+            .alert("Cannot Post Comment", error: $formViewModel.error)
+            .animation(.default, value: formViewModel.isWorking)
+            .disabled(formViewModel.isWorking)
+            .onSubmit(formViewModel.submit)
+            .onChange(of: formViewModel.isWorking) { isWorking in
+                guard !isWorking, formViewModel.error == nil else { return }
+                dismiss()
+            }
+        }
     }
 }
 
 #Preview {
-    CommentsList(commentsViewModel: CommentsViewModel(commentsRepository: CommentsRepositoryStub(state: .loaded([Comment.testComment]))))
+    NavigationStack {
+        CommentsList(commentsViewModel: CommentsViewModel(commentsRepository: CommentsRepositoryStub(state: .loaded([Comment.testComment]))))
+    }
 }
