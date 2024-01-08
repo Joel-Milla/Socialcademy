ddc97a3c46227a5469447adefba30a3c977aa260
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-08 12:08:18 -0600

    Update the repository to fetch posts of the user with their specific favorite status and not the other users status.

--- Socialcademy/Repositories/PostsRepository.swift
+++ Socialcademy/Repositories/PostsRepository.swift
@@ -22,6 +22,7 @@ protocol PostsRepositoryProtocol {
 
 struct PostsRepository: PostsRepositoryProtocol {
     let postsReference = Firestore.firestore().collection("posts")
+    let favoritesReference = Firestore.firestore().collection("favorites")
     
     func create(_ post: Post) async throws {
         let document = postsReference.document(post.id.uuidString)
@@ -41,27 +42,28 @@ struct PostsRepository: PostsRepositoryProtocol {
     }
     
     func favoriteAction(_ post: Post) async throws {
-        let document = postsReference.document(post.id.uuidString)
+        let favorite = Favorite(postID: post.id, userID: user.id)
+        let document = favoritesReference.document(favorite.id)
         if post.isFavorite {
-            try await document.setData(["isFavorite": false], merge: true)
+            try await document.delete()
         } else {
-            try await document.setData(["isFavorite": true], merge: true)
+            try await document.setData(from: favorite)
         }
     }
     
+    // Call the favorite posts of the current user in favorites (that contains the post id and author id...
+    // , and if it is not empty, then it fetches the posts content from postReference ...
+    // Then it returns the posts with the isFavorite variable with trye.
     func fetchFavoritePosts() async throws -> [Post] {
-        let query = postsReference
+        let favorites = try await fetchFavorites()
+        guard !favorites.isEmpty else { return [] }
+        let posts = try await postsReference
+            .whereField("id", in: favorites.map(\.uuidString))
             .order(by: "timestamp", descending: true)
-            .whereField("isFavorite", isEqualTo: true)
-        return try await fetchPosts(from: query)
-    }
-    
-    private func fetchPosts(from query: Query) async throws -> [Post] {
-        let snapshot = try await query.getDocuments()
-        let posts = snapshot.documents.compactMap { document in
-            try! document.data(as: Post.self)
+            .getDocuments(as: Post.self)
+        return posts.map { post in
+            post.setting(\.isFavorite, to: true)
         }
-        return posts
     }
     
     let user: User
@@ -78,6 +80,52 @@ struct PostsRepository: PostsRepositoryProtocol {
 extension PostsRepositoryProtocol {
     func canDelete(_ post: Post) -> Bool {
         post.author.id == user.id
+    }
+}
+
+// Extension of PostsRepository to fetch the correct favorite posts.
+private extension PostsRepository {
+    // fetch the posts of an author from posts and fetches favorites...
+    // then it returns the posts but if the post exist in favorites, save the variable...
+    // isFavorite as true. If not, then false.
+    private func fetchPosts(from query: Query) async throws -> [Post] {
+        let (posts, favorites) = try await (query.getDocuments(as: Post.self), fetchFavorites())
+        return posts.map { post in
+            post.setting(\.isFavorite, to: favorites.contains(post.id))
+        }
+    }
+    
+    // returns the posts in favorite that matches the current user id.
+    func fetchFavorites() async throws -> [Post.ID] {
+        return try await favoritesReference
+            .whereField("userID", isEqualTo: user.id)
+            .getDocuments(as: Favorite.self)
+            .map(\.postID)
+    }
+    
+    struct Favorite: Identifiable, Codable {
+        var id: String {
+            postID.uuidString + "-" + userID
+        }
+        let postID: Post.ID
+        let userID: User.ID
+    }
+}
+
+private extension Post {
+    func setting<T>(_ property: WritableKeyPath<Post, T>, to newValue: T) -> Post {
+        var post = self
+        post[keyPath: property] = newValue
+        return post
+    }
+}
+
+private extension Query {
+    func getDocuments<T: Decodable>(as type: T.Type) async throws -> [T] {
+        let snapshot = try await getDocuments()
+        return snapshot.documents.compactMap { document in
+            try! document.data(as: type)
+        }
     }
 }
 
