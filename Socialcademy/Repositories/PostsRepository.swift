8c3ced7c3a5f2cff4bc8387a26d93a7271edc341
Joel-Milla <jalejandro.milla@outlook.com>
2024-01-08 18:23:48 -0600

    Update the create and delete function of the repository to save the photo into firebase.

--- Socialcademy/Repositories/PostsRepository.swift
+++ Socialcademy/Repositories/PostsRepository.swift
@@ -25,6 +25,13 @@ struct PostsRepository: PostsRepositoryProtocol {
     let favoritesReference = Firestore.firestore().collection("favorites")
     
     func create(_ post: Post) async throws {
+        var post = post
+        if let imageFileURL = post.imageURL {
+            post.imageURL = try await StorageFile
+                .with(namespace: "posts", identifier: post.id.uuidString)
+                .putFile(from: imageFileURL)
+                .getDownloadURL()
+        }
         let document = postsReference.document(post.id.uuidString)
         try await document.setData(from: post)
     }
@@ -39,6 +46,8 @@ struct PostsRepository: PostsRepositoryProtocol {
         precondition(canDelete(post))
         let document = postsReference.document(post.id.uuidString)
         try await document.delete()
+        let image = post.imageURL.map(StorageFile.atURL(_:))
+        try await image?.delete()
     }
     
     func favoriteAction(_ post: Post) async throws {
